115ac08fb67a9ef1e0f5dabe9e4b569025d3ea13
kevdinh33 <kevdinh33@gmail.com>
2019-02-24 01:31:14 -0600

    Finished logic for Exercise 4.  Recoded for website navigation.

--- Exercise4/script.js
+++ Exercise4/script.js
@@ -4,7 +4,11 @@ function changer(){
   let r = document.getElementById("rValue").value;
   let g = document.getElementById("gValue").value;
   let b = document.getElementById("bValue").value;
-  let w = document.getElementById("width")
-  text.style.backgroundColor = "rgb("+r+", "+g+", "+b+")";
-  text.style.borderWidth = w;
+  let r2 = document.getElementById("r2Value").value;
+  let g2 = document.getElementById("g2Value").value;
+  let b2 = document.getElementById("b2Value").value;
+  let w = document.getElementById("width").value;
+  text.style.borderColor = "rgb("+r+", "+g+", "+b+")";
+  text.style.backgroundColor = "rgb("+r2+", "+g2+", "+b2+")";
+  text.style.borderWidth = w += "px";
 }
